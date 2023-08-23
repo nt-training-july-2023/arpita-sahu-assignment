fdfb1d8a9414c0c816331d0fd5e0de58dc5e2214
Arpita-NucleusTeq <arpita.sahu@nucleusteq.com>
2023-08-23 23:39:05 +0530

    registration and login API

---
+++ Capstone/Backend/AssessmentPlatform/src/main/java/com/nucleusteq/asessmentPlatform/entities/Category.java
@@ -0,0 +1,56 @@
+package com.nucleusteq.asessmentPlatform.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Category {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int categoryId; 
+
+    @Column(name = "category_title")
+    private String title;
+
+    @Column(name = "category_description")
+    private String description;
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int categoryId, String title, String description) {
+		super();
+		this.categoryId = categoryId;
+		this.title = title;
+		this.description = description;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
