0dc529796ccee6b1f527e6b870be80652fc5c19d
Arpita-NucleusTeq <arpita.sahu@nucleusteq.com>
2023-08-24 23:45:02 +0530

    category API added

--- Capstone/Backend/AssessmentPlatform/src/main/java/com/nucleusteq/asessmentPlatform/entities/Category.java
+++ Capstone/Backend/AssessmentPlatform/src/main/java/com/nucleusteq/asessmentPlatform/entities/Category.java
@@ -1,10 +1,14 @@
 package com.nucleusteq.asessmentPlatform.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="categories")
 public class Category {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,7 +33,7 @@ public class Category {
 		this.description = description;
 	}
 
-	public long getCategoryId() {
+	public int getCategoryId() {
 		return categoryId;
 	}
 
---
+++ Capstone/Backend/AssessmentPlatform/src/main/java/com/nucleusteq/asessmentPlatform/repositories/CategoryRepo.java
@@ -0,0 +1,10 @@
+package com.nucleusteq.asessmentPlatform.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nucleusteq.asessmentPlatform.entities.Category;
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+
+}
