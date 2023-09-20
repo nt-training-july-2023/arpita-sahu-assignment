2eff29ea63647da01f51080c13c71d0e87124b69
Arpita-NucleusTeq <arpita.sahu@nucleusteq.com>
2023-09-20 16:30:26 +0530

    take test modified

--- Capstone/Frontend/assessment-platform-frontend/src/Component/Question/TakeTest.js
+++ Capstone/Frontend/assessment-platform-frontend/src/Component/Question/TakeTest.js
@@ -1,38 +1,20 @@
-// import React, { useEffect, useState } from 'react'
-// import axios from 'axios';
-// import { useParams } from 'react-router-dom';
-
-// export default function TakeTest() {
-//     const[questions, setQuestions]= useState([]);
-//     const {quizId} =useParams();
-//     useEffect(()=>{
-//         loadQuestions();
-//       })
-    
-//           const loadQuestions = async () => {
-//             try {
-//               const result = await axios.get(`http://localhost:8080/ques/quiz/${quizId}`);
-//               setQuestions(result.data);
-//             } catch (error) {
-//               console.error("Error loading questions:", error);
-//             }
-//           };
-//   return (
-//     <div>TakeTest</div>
-//   )
-// }
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import "./question.css";
 
 export default function TakeTest() {
   const [questions, setQuestions] = useState([]);
-  const [selectedOptions, setSelectedOptions] = useState({}); // To store selected options
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+  const [quizTimer, setQuizTimer] = useState(null);
+  const [score, setScore] = useState(0);
   const { quizId } = useParams();
 
   useEffect(() => {
     loadQuestions();
-  }, [quizId]);
+    loadQuizTimer();
+  }, []);
 
   const loadQuestions = async () => {
     try {
@@ -43,38 +25,91 @@ export default function TakeTest() {
     }
   };
 
-  const handleOptionSelect = (questionId, option) => {
-    const updatedSelectedOptions = { ...selectedOptions };
-    updatedSelectedOptions[questionId] = option;
-    setSelectedOptions(updatedSelectedOptions);
+  const loadQuizTimer = async () => {
+    try {
+      const result = await axios.get(`http://localhost:8080/quiz/${quizId}`);
+      const quizEndTime = result.data.quizTimer;
+      const currentTime = new Date().getTime();
+      const timeRemaining = quizEndTime - currentTime;
+      if (timeRemaining > 0) {
+        setQuizTimer(
+          setTimeout(() => {
+            handleAutoSubmit();
+          }, timeRemaining)
+        );
+      }
+    } catch (error) {
+      console.error("Error loading quiz timer:", error);
+    }
+  };
+
+  const handleAnswerSelection = (questionId, selectedOption) => {
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: selectedOption,
+    }));
+  };
+
+  const handleAutoSubmit = () => {
+    if (!submitted) {
+      handleSubmit(new Event("autoSubmit"));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearTimeout(quizTimer);
+    let obtainedScore = 0;
+    questions.forEach((question) => {
+      const correctAnswer = question.answer;
+      const selectedAnswer = selectedAnswers[question.quesId];
+      if (selectedAnswer === correctAnswer) {
+        obtainedScore += 1;
+      }
+    });
+    setScore(obtainedScore);
+    setSubmitted(true);
   };
 
   return (
     <div>
       <h1>Take Test</h1>
-      <ul>
-        {questions.map((question) => (
-          <li key={question.id}>
-            <h3>{question.questionText}</h3>
-            <ul>
-              {question.options.map((option) => (
-                <li key={option.id}>
-                  <label>
-                    <input
-                      type="radio"
-                      name={`question_${question.id}`}
-                      value={option.id}
-                      checked={selectedOptions[question.id] === option.id}
-                      onChange={() => handleOptionSelect(question.id, option.id)}
-                    />
-                    {option.optionText}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      {questions.length > 0 ? (
+        <form onSubmit={handleSubmit}>
+          <div className="quiz-timer">Time Left: {formatTimeRemaining()}</div>
+          {questions.map((question, index) => (
+            <div className='question-card' key={index}>
+              <p>{index + 1} {question.question}</p>
+              <ul>
+                {['option1', 'option2', 'option3', 'option4'].map((optionKey) => (
+                  <p key={optionKey}>
+                    <label>
+                      <input
+                        type="radio"
+                        name={`question-${index}`}
+                        value={question[optionKey]}
+                        onChange={() => handleAnswerSelection(question.quesId, question[optionKey])}
+                        checked={selectedAnswers[question.quesId] === question[optionKey]}
+                        disabled={submitted}
+                      />
+                      {question[optionKey]}
+                    </label>
+                  </p>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <button type="submit">Submit Answers</button>
+        </form>
+      ) : (
+        <p>Loading questions...</p>
+      )}
+      {submitted && (
+        <div>
+          <h2>Your Score:</h2>
+          <p>{score} out of {questions.length}</p>
+        </div>
+      )}
     </div>
   );
 }
